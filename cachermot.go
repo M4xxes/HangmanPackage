d88a7e4aa3a040573b35b5566c0646df052bc157
pemaxime <maxxes@macbook-air-de-maxime.home>
2024-01-10 21:44:51 +0100

    modif cachermot

--- cachermot.go
+++ cachermot.go
@@ -1,16 +1,25 @@
 package hangmanpackage
 
-import "math/rand"
+import (
+	"math/rand"
+)
 
 func CacherMot(mot string) string {
+	if len(mot) == 0 {
+		// Retourner une chaîne vide ou gérer l'erreur si le mot est vide
+		return ""
+	}
+
 	n := len(mot)/2 - 1
 	if n < 1 {
 		n = 1
 	}
+
 	motCache := make([]byte, len(mot))
 	for i := range mot {
 		motCache[i] = '_'
 	}
+
 	for i := 0; i < n; i++ {
 		// Révéler une lettre aléatoire
 		for {
